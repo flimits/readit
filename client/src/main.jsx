5914a392e2343961fd0a7c2ab86c5342864f9efd
Jason Savoie <flimits@gmail.com>
2023-12-02 11:16:03 -0800

    starter client code to start up. need to clean this up and add stuff for eadit

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App.jsx'
+import Home from './pages/Home'
+import Error from './pages/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
