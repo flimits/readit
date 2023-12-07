c3b8e943baceefbe5ec6b792aaf90dd28fd4262e
Jacastro619 <jorgecastro619@gmail.com>
2023-12-06 18:12:42 -0800

    added header ui with nav bar

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import NavTabs from "./navTabs";
+
+export default function Header() {
+  return (
+    <header>
+      <NavTabs />
+    </header>
+  );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-import App from './App.jsx'
-import Home from './pages/Home'
-import Error from './pages/Error'
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
@@ -19,6 +21,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
