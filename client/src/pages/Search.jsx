9cd6114754004e86c9eef57898649801bcc62890
Nathan Geronimo <nathangero@yahoo.com>
2023-12-08 12:53:20 -0800

    implement searching for posts

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,61 @@
-export default function Search()  {
-    return (
-        <div>
-            Hello Search Page!!
+import { useEffect, useState } from "react"
+import { useLazyQuery, useQuery } from '@apollo/client';
+import { SEARCH_POSTS } from '../utils/queries'
+
+export default function Search() {
+  const [searchPosts, { data }] = useLazyQuery(SEARCH_POSTS);
+
+  const [searchQuery, setSearch] = useState('');
+  const [searchTitle, setTitle] = useState(true);
+  const [searchText, setText] = useState(true);
+  const [searchTags, setTags] = useState(true);
+
+  useEffect(() => {
+    if (data) {
+      console.log("posts:", data);
+    }
+  }, [data]);
+
+  const handleOnChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    console.log("query:", searchQuery)
+
+    try {
+      await searchPosts({
+        variables: {
+          query: searchQuery,
+          useTitle: searchTitle,
+          useText: searchText,
+          useTags: searchTags,
+        }
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <section className="d-flex justify-content-center mt-3">
+      <form className="w-75" role="search" onSubmit={handleOnSubmit}>
+        <label htmlFor="searchInput" className="form-label pull-end">What do you want to search for?</label>
+
+        <div className="input-group">
+          <input
+            id="searchInput"
+            className="form-control me-2"
+            value={searchQuery}
+            onChange={handleOnChange}
+            type="search"
+            placeholder="e.g. baking golf programming"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">Search</button>
         </div>
-    )
+      </form>
+    </section>
+  )
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -12,3 +12,22 @@ export const QUERY_THOUGHTS = gql`
     }
   }
 `;
+
+
+export const SEARCH_POSTS = gql`
+query SearchPosts($query: String!, $useTitle: Boolean, $useText: Boolean, $useTags: Boolean) {
+  searchPosts(query: $query, useTitle: $useTitle, useText: $useText, useTags: $useTags) {
+    _id
+    userId
+    title
+    postText
+    tags
+    reactions {
+      applause
+    }
+    comments {
+      text
+    }
+  }
+}
+`;
