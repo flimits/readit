4c3bf0d86cd064832c543e04d423db6b51fb4b58
Dee <deepakdilse@gmail.com>
2023-12-09 16:14:42 -0800

    disable post title being clickable on view-post page

--- client/src/pages/ViewPost.jsx
+++ client/src/pages/ViewPost.jsx
@@ -8,16 +8,12 @@ import { QUERY_SINGLE_POST } from "../utils/queries";
 
 const ViewPost = () => {
   const { postId } = useParams();
-  //console.log("View post with params: ", postId);
   const { loading, data } = useQuery(QUERY_SINGLE_POST, {
     //pass url params
     variables: { postId: postId },
   });
 
   if (loading) return "Loading...";
-
-  console.log("View post with params: ", data?.getPost);
-
   const post = data?.getPost || {};
 
   return (
