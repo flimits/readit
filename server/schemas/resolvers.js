2b2cdb8a80dcd5f5884e572528f2a80bd32b09e0
Jason Savoie <flimits@gmail.com>
2023-12-12 11:06:22 -0800

    fixing spacing on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,32 +25,31 @@ const resolvers = {
         console.error(error);
       }
     },
-getMe: async (parent, args, context) => {
-        // TODO: Test with frontend
+    getMe: async (parent, args, context) => {
       // If we know the logged in user is wanting to look at their account, use context
-  try {
-    console.log("context.user:", context.user);
+      try {
+        console.log("context.user:", context.user);
 
-    if (context.user) {
-      const user = await User
-        .findById(new ObjectId(context.user._id))
-        .populate({
-          path: "posts",
-          populate: {
-            path: "author",
-            select: "userName",
-          },
-        });
+        if (context.user) {
+          const user = await User
+            .findById(new ObjectId(context.user._id))
+            .populate({
+              path: "posts",
+              populate: {
+                path: "author",
+                select: "userName",
+              },
+            });
 
-      console.log("user:", user);
-      return user;
-    }
+          // console.log("user:", user);
+          return user;
+        }
 
-    throw ErrorAuthentication;
-  } catch (error) {
-    console.error("Error in getMe resolver:", error);
-  }
-},
+        throw ErrorAuthentication;
+      } catch (error) {
+        console.error("Error in getMe resolver:", error);
+      }
+    },
 
     posts: async () => {
       try {
