851e46213375e2dcf0ac43e3510346e281d1172e
Jason Savoie <flimits@gmail.com>
2023-12-12 11:46:40 -0800

    Adding also sorting for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,12 +9,22 @@ const Home = () => {
   if (loading) return "Loading ...";
   if (error) return `Error ! ${error.message}`;
 
+  // Lets sort the posts by createdAt in descending order
+  const sortedPosts = data?.posts.slice().sort((a,b) => {
+    // you can change the sort direction with '>' to '<' for ascending order
+    return new Date(b.createdAt) - new Date(a.createdAt);
+  });
+  
   return (
     <main className="container">
       <div className="flex-row justify-center">
         <div className="col-12 mb-3">
-          {data?.posts.map((item) => (
-            <Post post={item} key={item._id} />
+        {sortedPosts.map((item) => (
+            <Post
+              post={item}
+              key={item._id}
+              username={item?.author?.userName}
+            />
           ))}
         </div>
       </div>
